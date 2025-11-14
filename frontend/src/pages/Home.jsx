68b9c3b42314a8404597c4a717e8d9e7ead14875
furqan <mohammedfurqanpashaa@gmail.com>
2025-11-14 16:20:36 +0530

    Create Home.jsx

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import App from '../App';
+
+// In a real application, this file would typically contain the main logic/layout.
+// Since we put all the logic into App.jsx for the prototype, this acts as a wrapper.
+
+function Home() {
+    return (
+        <App />
+    );
+}
+
+export default Home;
